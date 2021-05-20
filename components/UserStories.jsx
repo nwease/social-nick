c9133c02446ae31c78df0e0a6242fe6f2cda9ecf
Nicholas Wease <nicholaswease@Nicholass-MacBook-Air.local>
2021-05-20 12:14:44 +0200

    Working on StoryCard

--- components/UserStories.jsx
+++ components/UserStories.jsx
@@ -1,7 +1,45 @@
+import StoryCard from './StoryCard';
+
+const stories = [
+    {
+        name: 'Sonny Sangha',
+        src: 'https://links.papareact.com/zof',
+        profile: 'https://links.papareact.com/l4v',
+    },
+    {
+        name: 'Elon Musk',
+        src: 'https://links.papareact.com/4zn',
+        profile: 'https://links.papareact.com/kxk',
+    },
+    {
+        name: 'Jeff Bezos',
+        src: 'https://links.papareact.com/k2j',
+        profile: 'https://links.papareact.com/f0p',
+    },
+    {
+        name: 'Mark Zuckerberg',
+        src: 'https://links.papareact.com/xql',
+        profile: 'https://links.papareact.com/snf',
+    },
+    {
+        name: 'Bill Gates',
+        src: 'https://links.papareact.com/4u4',
+        profile: 'https://links.papareact.com/zvy',
+    },
+];
+
 function UserStories() {
     return (
-        <div>
-            USER STORIES
+        <div className='flex justify-center space-x-4 mx-auto'>
+            {
+                stories.map(story => (
+                    <StoryCard
+                        name={story.name}
+                        src={story.src}
+                        profile={story.profile}
+                    />
+                ))
+            }
         </div>
     );
 }
